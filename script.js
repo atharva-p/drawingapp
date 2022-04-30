9eda306adb40f714d4d002b8c9a527a8c3ed7920
Atharva Patil <65209968+atharva-p@users.noreply.github.com>
2022-04-30 23:32:52 +0530

    changed canvas test again

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 function resize() {
-  canvas.width = window.innerWidth;
   canvas.height = window.innerHeight - 60;
+  canvas.width = window.innerWidth;
 }
 resize();
 
+//canvas test
 ctx.beginPath();
 ctx.moveTo(20, 20);
-ctx.lineTo(40, 20);
+ctx.lineTo(50, 20);
+ctx.lineTo(50, 50);
+ctx.stroke();
+ctx.closePath();
 ctx.stroke();
