6d010dbf1dc8f88658db845431121c535e15634a
Atharva Patil <65209968+atharva-p@users.noreply.github.com>
2022-05-10 14:39:35 +0530

    added a console log (temp)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
+console.log("hello kavipriya");
 
 //for sizing the canvas when opened or changed
 function resize() {
